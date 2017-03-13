34d2ee58f34ef6d359ac8548d5e7a90cdbc3ad1c
gudiyaa <shivanky1139@gmail.com>
2017-03-13 20:28:59 +0530

    wsgi settings for pythonanywhere.com

--- pro/wsgi.py
+++ pro/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/Shivanky/myblog'  
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myblog.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pro.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
