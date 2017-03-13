f56861f6b5cd90cbd9bea83421f1393ec1127e5c
gudiyaa <shivanky1139@gmail.com>
2017-03-13 20:17:38 +0530

    Test on models.py

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-# Create your models here.
-# class SignUp(models.Model):
-# 	email=models.EmailField(max_length=200)
-# 	full_name=models.CharField(max_length=100,blank=True,null=True)
-# 	timestamp=models.DateTimeField(auto_now=True)	
-
-# 	def __str__(self):
-# 		return self.email
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
