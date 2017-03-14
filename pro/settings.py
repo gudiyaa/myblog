0deb54f35671618f4a019bfc4ec78d224dc1f34c
gudiyaa <shivanky1139@gmail.com>
2017-03-14 16:32:23 +0530

    settings file changed

--- pro/settings.py
+++ pro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j3n8k_s*t&x@_%&mv%(6y&3#*ut+oy-oqf_erxliu6146ru^k1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Shivanky$myblog',
         'HOST': 'Shivanky.mysql.pythonanywhere-services.com',
         'USER' : 'Shivanky'
         'PASSWORD' : 'gudiya123'
--- pro/wsgi.py
+++ pro/wsgi.py
@@ -8,14 +8,14 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-import sys
+try:
+	import pymysql
+	pymysql.install_as_MySQLdb()
+except ImportError:
+	pass
 
-path = '/home/Shivanky/myblog'  
-if path not in sys.path:
-    sys.path.append(path)
+from django.core.wsgi import get_wsgi_applications
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myblog.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","pro.settings")
 
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+aplication=get_wsgi_application()
