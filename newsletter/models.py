0c65d3ac3b9af7bde3c89b0510794faaa4e8532d
gudiyaa <shivanky1139@gmail.com>
2017-03-14 17:55:40 +0530

    database changed

--- newsletter/models.py
+++ newsletter/models.py
@@ -4,8 +4,8 @@ from django.conf import settings
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
-	# author = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-	author = models.ForeignKey('request.user')
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+	# author = models.ForeignKey('request.user')
 	body = models.TextField()
 	create_date = models.DateTimeField(auto_now=True)
 
