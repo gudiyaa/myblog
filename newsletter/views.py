0a0635344efe58d850a59f883edc8a8a70076a69
gudiyaa <shivanky1139@gmail.com>
2017-03-19 01:30:27 +0530

    author changed

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-from .forms import ContactForm,BlogForm
+from .forms import BlogForm
 from django.conf import settings
-from django.core.mail import send_mail
 from .models import Post
 # Create your views here.
 class Home(View):
