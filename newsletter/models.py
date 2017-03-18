1842f0fecaccef634a1eb427eaea6bc8311ea316
gudiyaa <shivanky1139@gmail.com>
2017-03-18 12:50:19 +0530

    settings changed

--- newsletter/models.py
+++ newsletter/models.py
@@ -5,7 +5,6 @@ from django.conf import settings
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-	# author = models.ForeignKey('request.user')
 	body = models.TextField()
 	create_date = models.DateTimeField(auto_now=True)
 
